7d1be62f0bdfe37d2db220542d16cf4ab12d2698
Andri Winata <3tagger95@gmail.com>
2024-10-05 15:07:35 +0700

    feat: using stored data to display menu content

--- public/main.php
+++ public/main.php
@@ -1,9 +1,17 @@
 <?php
+
 require __DIR__ . '/../vendor/autoload.php';
 
+use League\Flysystem\Local\LocalFilesystemAdapter;
 use Symfony\Component\HttpFoundation\Request;
 use Twig\Loader\FilesystemLoader;
 use Twig\Environment;
+use League\Flysystem\Filesystem;
+
+$adapter = new LocalFilesystemAdapter(__DIR__ . '/../data');
+$filesystem = new Filesystem($adapter);
+
+$variables = json_decode($filesystem->read('menu.json'), true);
 
 $request = Request::createFromGlobals();
 $name = $request->get('name');
@@ -11,4 +19,4 @@ $name = $request->get('name');
 $loader = new FilesystemLoader(__DIR__ . '/../templates');
 $twig = new Environment($loader);
 
-echo $twig->render('index.html', ['name' => $name]);
+echo $twig->render('index.html', $variables['index']);
