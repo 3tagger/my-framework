5ad7b5b768d7a051f99bd085d93c96959f9bdcab
Andri Winata <3tagger95@gmail.com>
2024-10-05 13:01:35 +0700

    feat: initial commit

---
+++ src/main.php
@@ -0,0 +1,14 @@
+<?php
+    require __DIR__ . '/../vendor/autoload.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My PHP Framework</title>
+</head>
+<body>
+    <h1>Hi there!</h1>
+</body>
+</html>
